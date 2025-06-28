a0b17e24a85b018cebe97417f8c32d5ea41f28cf
Yoav Mlotok <yoavmlotok@icloud.com>
2025-06-28 11:31:41 +0300

    Inital commit

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = { flavour = "mocha" },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
